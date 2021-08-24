94890d77829ae462f1ce6c6d9b1aaec977200ceb
Sherine Darwish <sheriinekhaled97@gmail.com>
2021-08-24 22:11:09 +0200

    port update

--- app.js
+++ app.js
@@ -92,5 +92,5 @@ module.exports = app;
 
 
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, console.log(`Server running on ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server running on ${PORT}`));
